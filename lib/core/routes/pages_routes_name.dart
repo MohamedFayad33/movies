c6fb7201006491283c2f37ef6fe174777c07164b
mohamed <eng.mohamed.fayad33@gmail.com>
2025-09-03 23:08:41 +0400

    create rout file

---
+++ lib/core/routes/pages_routes_name.dart
@@ -0,0 +1,8 @@
+abstract class PagesRoutesName {
+  static const String splash = '/';
+  static const String onBoarding = "/onBoarding_view";
+  static const String login = '/Login';
+  static const String register = '/register';
+  static const String forget = 'forget';
+  static const String layout = 'layout';
+}
---
+++ lib/core/routes/route.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class Routes {
+  static Map<String, Widget Function(BuildContext)> routes = {};
+}
