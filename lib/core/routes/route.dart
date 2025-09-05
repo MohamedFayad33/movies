29ad3e0845c58e34dccf62dcd39c9d074023812f
mohamedsamir00010 <mohamedcamirr@gmail.com>
2025-09-05 16:42:23 +0300

    create route.dart

--- lib/core/routes/route.dart
+++ lib/core/routes/route.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/core/routes/pages_routes_name.dart';
-import 'package:movies_app/modules/auth/login_screen.dart';
+import 'package:movies_app/modules/login_view/login_view.dart';
 import 'package:movies_app/modules/onBoarding_view/on_boarding_screen_view.dart';
 
 
 abstract class Routes {
   static Map<String, Widget Function(BuildContext)> routes = {
     PagesRoutesName.onBoarding: (context) => const OnBoardingScreenView(),
-    PagesRoutesName.login: (context) => const LoginScreen()
+    PagesRoutesName.login: (context) => const LoginView()
   };
 }
+
 
