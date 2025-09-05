8f27576018d8877c0ee718f5f57a13bdec3e592e
mohamedsamir00010 <mohamedcamirr@gmail.com>
2025-09-05 16:35:15 +0300

    create pages_routes_name

--- lib/core/routes/pages_routes_name.dart
+++ lib/core/routes/pages_routes_name.dart
@@ -1,8 +1,5 @@
 abstract class PagesRoutesName {
-  static const String splash = '/';
   static const String onBoarding = "/onBoarding_view";
   static const String login = '/Login';
-  static const String register = '/register';
-  static const String forget = 'forget';
-  static const String layout = 'layout';
 }
+
