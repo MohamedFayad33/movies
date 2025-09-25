7c71c1269b5952d8d20ca15803c3b11752e519ef
mohamedsamir00010 <mohamedcamirr@gmail.com>
2025-09-25 20:43:05 +0300

    create browse_view

--- lib/modules/layout/browse/browse_view.dart
+++ lib/modules/layout/browse/browse_view.dart
@@ -1,127 +1,108 @@
-
 import 'package:flutter/material.dart';
 import 'package:movies_app/core/constant/assets/assets.dart';
+
 class BrowseView extends StatelessWidget {
   final List<String> categories = ["Action", "Adventure", "Animation", "Biography"];
+
   final List<Map<String, dynamic>> movies = [
-    {
-      "rating": 7.7,
-      "image": Assets.movie1
-    },
-    {
-      "rating": 7.7,
-      "image": Assets.movie2
-    },
-    {
-      "rating": 7.7,
-      "image":Assets.movie3
-    },
-    {
-      "rating": 7.7,
-      "image": Assets.movie4
-    },
-    {
-      "rating": 7.7,
-      "image": Assets.movie5
-    },
-    {
-      "rating": 7.7,
-      "image": Assets.movie6
-    }
+    {"rating": 7.7, "image": Assets.movie1},
+    {"rating": 7.7, "image": Assets.movie2},
+    {"rating": 7.7, "image": Assets.movie3},
+    {"rating": 7.7, "image": Assets.movie4},
+    {"rating": 7.7, "image": Assets.movie5},
+    {"rating": 7.7, "image": Assets.movie6}
   ];
-   BrowseView({super.key});
-  
+
+  BrowseView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      backgroundColor: Color(0xFF121312),
-      body: SafeArea(
-        child: Padding(padding: EdgeInsets.only(top: 12),
-          child: DefaultTabController(
-            length: 4,
-            child: Column(
-              children: [
-                TabBar(
-                  tabAlignment: TabAlignment.start,
-                  dividerColor: Colors.transparent,
-                  isScrollable: true,
-                  labelPadding: EdgeInsets.symmetric(horizontal: 4),
-                  indicator: BoxDecoration(
-                    color: Color(0xFFF6BD00), 
+    return DefaultTabController(
+      length: categories.length,
+      child: Scaffold(
+        backgroundColor: const Color(0xFF121312),
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF121312),
+          toolbarHeight: 0,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(65),
+            child: TabBar(
+              tabAlignment: TabAlignment.start,
+              dividerColor: Colors.transparent,
+              isScrollable: true,
+              labelPadding: const EdgeInsets.symmetric(horizontal: 4),
+              indicator: BoxDecoration(
+                color: const Color(0xFFF6BD00),
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: const Color(0xFFF6BD00), width: 3),
+              ),
+              labelColor: Colors.black,
+              unselectedLabelColor: const Color(0xFFF6BD00),
+              tabs: List.generate(
+                categories.length,
+                    (index) => Container(
+                  height: 55,
+                  padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: const Color(0xFFF6BD00), width: 3),
                     borderRadius: BorderRadius.circular(20),
-                    border: Border.all(color: Color(0xFFF6BD00), width: 3),
                   ),
-                  labelColor: Colors.black,
-                  unselectedLabelColor: Color(0xFFF6BD00), 
-                  tabs: List.generate(
-                    categories.length,
-                        (index) => Container(
-                      height: 55,
-                      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Color(0xFFF6BD00), width: 3),
-                        borderRadius: BorderRadius.circular(20),
+                  child: Center(
+                    child: Text(
+                      categories[index],
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
                       ),
-                      child: Center(
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: List.generate(
+            categories.length,
+                (index) => GridView.builder(
+              padding: const EdgeInsets.all(12),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                childAspectRatio: 0.65,
+              ),
+              itemCount: movies.length,
+              itemBuilder: (context, index) {
+                return Stack(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.asset(
+                          movies[index]["image"],
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 10,
+                      left: 8,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black54,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: const EdgeInsets.all(5),
                         child: Text(
-                          categories[index],
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
-                          ),
+                          " ${movies[index]["rating"]} ⭐",
+                          style: const TextStyle(color: Colors.white),
                         ),
                       ),
                     ),
-                  ),
-                ),
-
-                SizedBox(height: 8),
-                Expanded(
-                  child: TabBarView(
-                    children: List.generate(categories.length, (index) =>
-                        GridView.builder(
-                          padding: EdgeInsets.all(12),
-                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            crossAxisSpacing: 12,
-                            childAspectRatio: 0.65,
-                          ),
-                          itemCount: movies.length,
-                          itemBuilder: (context, index) {
-                            return Stack(
-                              children: [
-                                InkWell(onTap: (){},
-                                  child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(12),
-                                  child: Image.asset(
-                                    movies[index]["image"],
-                                    fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                                Positioned(
-                                  top: 10,
-                                  left: 8,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.black54,
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    padding: EdgeInsets.all(5),
-                                    child: Text(
-                                      " ${movies[index]["rating"]} ⭐",
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            );
-                          },
-                        ),
-                    ),
-                  ),
-                ),
-              ],
+                  ],
+                );
+              },
             ),
           ),
         ),
